063c6544190eb8a94aa7d80ec5667e458520f1e0
VanHakobyan <van19962013@mail.ru>
2017-01-16 20:00:47 +0400

    Add Extension method

---
+++ Exception/Exception/symmetrical.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class symmetrical
+    {
+        public static bool isSymmetrical(this string word)
+        {
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != word[word.Length-1 - i])
+                    return false;
+
+            }
+            return true;
+
+        }
+    }
+}
---
+++ Exception/Testing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Extension;
+namespace Testing
+{
+    class Program
+    {
+        static void Main()
+        {
+            string st1 = "Anna";
+            string st2 = "AramarA";
+            string st3 = "aNNa";
+            string st4 = "ՀայաՀ";
+            string st5 = Console.ReadLine();
+            Console.WriteLine(st1.isSymmetrical());//false
+            Console.WriteLine(st2.isSymmetrical());//true
+            Console.WriteLine(st3.isSymmetrical());//true
+            Console.WriteLine(st4.isSymmetrical());//true
+            Console.WriteLine(st5.isSymmetrical());
+
+        }
+    }
+}
