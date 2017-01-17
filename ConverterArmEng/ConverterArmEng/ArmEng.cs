e01bb084bbfead207a1a04ab61866f89931476d9
Hasmik Mikaelyan <hasmik.mikaelyan.hm@gmail.com>
2017-01-17 13:36:12 +0400

    add eng

--- ConverterArmEng/ConverterArmEng/ArmEng.cs
+++ ConverterArmEng/ConverterArmEng/ArmEng.cs
@@ -14,7 +14,7 @@ namespace ConverterArmEng
             for (int i = 0; i < word.Length; i++)
             {
                 //a	b	c	d	e	f	g	h	i	j	k	l	m	n	o	p	q	r	s	t	u	v	w	x	y	z
-
+                #region armEng
                 if (word[i] == 'է') arm[i] = '1';
                 if (word[i] == 'թ') arm[i] = '2';
                 if (word[i] == 'փ') arm[i] = '3';
@@ -57,8 +57,55 @@ namespace ConverterArmEng
                 if (word[i] == '-') arm[i] = '-';
                 if (word[i] == '`') arm[i] = '`';
                 if (word[i] == ',') arm[i] = ',';
-                
-                
+
+                #endregion
+
+                #region engArm
+                if (word[i] == '1') arm[i] = 'է';
+                if (word[i] == '2') arm[i] = 'թ';
+                if (word[i] == '3') arm[i] = 'փ';
+                if (word[i] == '4') arm[i] = 'ձ';
+                if (word[i] == '5') arm[i] = 'ջ';
+                if (word[i] == '7') arm[i] = 'և';
+                if (word[i] == '8') arm[i] = 'ր';
+                if (word[i] == '9') arm[i] = 'չ';
+                if (word[i] == '0') arm[i] = 'ճ';
+                if (word[i] == '=') arm[i] = 'ժ';
+                if (word[i] == 'q') arm[i] = 'ք';
+                if (word[i] == 'w') arm[i] = 'ո';
+                if (word[i] == 'e') arm[i] = 'ե';
+                if (word[i] == 'r') arm[i] = 'ռ';
+                if (word[i] == 't') arm[i] = 'տ';
+                if (word[i] == 'y') arm[i] = 'ը';
+                if (word[i] == 'u') arm[i] = 'ւ';
+                if (word[i] == 'i') arm[i] = 'ի';
+                if (word[i] == 'o') arm[i] = 'օ';
+                if (word[i] == 'p') arm[i] = 'պ';
+                if (word[i] == '[') arm[i] = 'խ';
+                if (word[i] == ']') arm[i] = 'ծ';
+                if (word[i] == 'a') arm[i] = 'ա';
+                if (word[i] == 's') arm[i] = 'ս';
+                if (word[i] == 'd') arm[i] = 'դ';
+                if (word[i] == 'f') arm[i] = 'ֆ';
+                if (word[i] == 'g') arm[i] = 'գ';
+                if (word[i] == 'h') arm[i] = 'հ';
+                if (word[i] == 'j') arm[i] = 'յ';
+                if (word[i] == 'k') arm[i] = 'կ';
+                if (word[i] == 'l') arm[i] = 'լ';
+                if (word[i] == 'z') arm[i] = 'զ';
+                if (word[i] == 'x') arm[i] = 'ղ';
+                if (word[i] == 'c') arm[i] = 'ց';
+                if (word[i] == 'v') arm[i] = 'վ';
+                if (word[i] == 'b') arm[i] = 'բ';
+                if (word[i] == 'n') arm[i] = 'ն';
+                if (word[i] == 'm') arm[i] = 'մ';
+                if (word[i] == ' ') arm[i] = ' ';
+                if (word[i] == '-') arm[i] = '-';
+                if (word[i] == '`') arm[i] = '`';
+                if (word[i] == ',') arm[i] = ',';
+
+                #endregion
+
             }
             return arm;
         }
--- ConverterArmEng/Testing/Program.cs
+++ ConverterArmEng/Testing/Program.cs
@@ -6,9 +6,11 @@ namespace Testing
     {
         static void Main(string[] args)
         {
-            string eng = "ես տանն եմ";
-            
+            string arm = "ես տանն եմ";
+            string eng = "es tann em";
             Console.WriteLine(eng.ConvertEng()); //es tann em
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(arm.ConvertEng());
         }
     }
 }
