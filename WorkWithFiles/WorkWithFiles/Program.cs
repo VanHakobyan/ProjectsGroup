245b1201dcf6fa99cb89b7692ff4354457b022fb
VanHakobyan <van19962013@mail.ru>
2017-01-18 15:24:42 +0400

    1st push

---
+++ WorkWithFiles/WorkWithFiles/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace WorkWithFiles
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Input the file name !");
+            string fileName = Console.ReadLine();
+            Console.WriteLine(File.ReadAllText(fileName)) ;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WorkWithFiles/WorkWithFiles01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+namespace WorkWithFiles01
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            using (var sw = File.AppendText(@"D:\text.txt"))
+            {
+                sw.WriteLine("Input text text.txt");
+            }
+        }
+    }
+}
