b9aafcb18667e6272bdf0d7bf7df539d85089021
VanHakobyan <van19962013@mail.ru>
2017-01-18 22:46:38 +0400

    add exception

---
+++ stackOverFlowException/stackOverFlowException/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace stackOverFlowException
+{
+    class Program
+    {
+        public static void method()
+        {
+            int[] array = new int[100];
+            Console.WriteLine(array);
+            method(); //recursion
+        }
+
+        static void Main()
+        {
+            try
+            {
+                method();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {   
+                //finally does not work
+                Console.WriteLine("i am finally");
+            }
+        }
+    }
+}
