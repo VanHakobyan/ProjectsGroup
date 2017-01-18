0c9b14f196525fe5da951ea190d5332b7d027182
VanHakobyan <van19962013@mail.ru>
2017-01-18 18:19:10 +0400

    add FileStream

---
+++ WorkWithFiles/WorkWithFiles02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace WorkWithFiles02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create file
+            FileStream file = File.Create(@"D:\textNew.txt");
+
+            var writer = new BinaryWriter(file);
+
+            // Write in different data file.
+            long number = 1000;
+            int[] num = new int[number];
+            string st = "funy c#";
+
+            writer.Write(number);
+            writer.Write(st);
+            for (int i = 0; i < num.Length; i++)
+            {
+                writer.Write(num[i]);
+            }
+
+            // Delete
+            writer.Close();
+        }
+    }
+}
